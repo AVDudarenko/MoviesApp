49a52f060a4fb9f7597f141b1c18c8ca9b86a51f
Aliaksandr <dyvedd@gmail.com>
2023-11-02 14:49:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moviesapp/pojo/Rating.java
@@ -0,0 +1,23 @@
+package com.example.moviesapp.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+	@SerializedName("kp")
+	private String kp;
+	@SerializedName("imdb")
+	private String imdb;
+
+	public Rating(String ratingKp, String ratingImdb) {
+		this.kp = ratingKp;
+		this.imdb = ratingImdb;
+	}
+
+	public String getKp() {
+		return kp;
+	}
+
+	public String getImdb() {
+		return imdb;
+	}
+}
