8b64769936c17244eafc82664717b46f9ad5d3ee
Aliaksandr <dyvedd@gmail.com>
2023-11-02 16:43:14 +0300

    add live data and query for URL

--- app/src/main/java/com/example/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -1,19 +1,12 @@
 package com.example.moviesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 import android.util.Log;
 
-import com.example.moviesapp.pojo.MovieResponse;
-import com.example.moviesapp.service.ApiService;
-import com.example.moviesapp.view.model.MainViewModel;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Scheduler;
-import io.reactivex.rxjava3.functions.Consumer;
-import io.reactivex.rxjava3.schedulers.Schedulers;
+import com.example.moviesapp.view.model.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
