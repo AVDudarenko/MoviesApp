20f9a4615090ff73f99fdc32d3de1bc205ece918
Aliaksandr <dyvedd@gmail.com>
2023-11-08 11:35:01 +0300

    fix error with Room, add menu

--- app/src/main/java/com/example/moviesapp/activity/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/activity/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.moviesapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -16,44 +19,59 @@ import com.example.moviesapp.view.model.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainViewModel mainViewModel;
-    private RecyclerView recyclerViewMovies;
-    private ProgressBar pbLoading;
-    private MoviesAdapter moviesAdapter;
+	private MainViewModel mainViewModel;
+	private RecyclerView recyclerViewMovies;
+	private ProgressBar pbLoading;
+	private MoviesAdapter moviesAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        initViews();
+		initViews();
 
-        moviesAdapter = new MoviesAdapter();
-        recyclerViewMovies.setAdapter(moviesAdapter);
-        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
+		moviesAdapter = new MoviesAdapter();
+		recyclerViewMovies.setAdapter(moviesAdapter);
+		recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
 
-        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        mainViewModel.getMoviesLiveData().observe(
-                this,
-                movies -> moviesAdapter.setMovies(movies));
-        mainViewModel.getIsLoading().observe(
-                this,
-                isLoading -> {
-                    if (isLoading) {
-                        pbLoading.setVisibility(View.VISIBLE);
-                    } else {
-                        pbLoading.setVisibility(View.GONE);
-                    }
-                });
-        moviesAdapter.setOnReachEndListener(() -> mainViewModel.loadMovies());
-        moviesAdapter.setOnMovieClickListener(movie -> {
-            Intent intent = MovieDetailActivity.newIntent(MainActivity.this, movie);
-            startActivity(intent);
-        });
-    }
+		mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+		mainViewModel.getMoviesLiveData().observe(
+				this,
+				movies -> moviesAdapter.setMovies(movies));
+		mainViewModel.getIsLoading().observe(
+				this,
+				isLoading -> {
+					if (isLoading) {
+						pbLoading.setVisibility(View.VISIBLE);
+					} else {
+						pbLoading.setVisibility(View.GONE);
+					}
+				});
+		moviesAdapter.setOnReachEndListener(() -> mainViewModel.loadMovies());
+		moviesAdapter.setOnMovieClickListener(movie -> {
+			Intent intent = MovieDetailActivity.newIntent(MainActivity.this, movie);
+			startActivity(intent);
+		});
+	}
 
-    private void initViews() {
-        recyclerViewMovies = findViewById(R.id.rvMovies);
-        pbLoading = findViewById(R.id.pbLoading);
-    }
+	private void initViews() {
+		recyclerViewMovies = findViewById(R.id.rvMovies);
+		pbLoading = findViewById(R.id.pbLoading);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (item.getItemId() == R.id.item_favorites) {
+			Intent intent = FavoriteMovieActivity.newIntent(MainActivity.this);
+			startActivity(intent);
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- app/src/main/java/com/example/moviesapp/pojo/Rating.java
+++ app/src/main/java/com/example/moviesapp/pojo/Rating.java
@@ -7,30 +7,41 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 public class Rating implements Serializable {
-    @SerializedName("kp")
-    private final double kp;
-    @SerializedName("imdb")
-    private final double imdb;
+	@SerializedName("kp")
+	double kp;
+	@SerializedName("imdb")
+	double imdb;
 
-    public Rating(double ratingKp, double ratingImdb) {
-        this.kp = ratingKp;
-        this.imdb = ratingImdb;
-    }
+	public Rating(double ratingKp, double ratingImdb) {
+		this.kp = ratingKp;
+		this.imdb = ratingImdb;
+	}
 
-    public double getKp() {
-        return kp;
-    }
+	public Rating() {
+	}
 
-    public double getImdb() {
-        return imdb;
-    }
+	public double getKp() {
+		return kp;
+	}
 
-    @NonNull
-    @Override
-    public String toString() {
-        return "Rating{" +
-                "kp='" + kp + '\'' +
-                ", imdb='" + imdb + '\'' +
-                '}';
-    }
+	public double getImdb() {
+		return imdb;
+	}
+
+	public void setKp(double kp) {
+		this.kp = kp;
+	}
+
+	public void setImdb(double imdb) {
+		this.imdb = imdb;
+	}
+
+	@NonNull
+	@Override
+	public String toString() {
+		return "Rating{" +
+				"kp='" + kp + '\'' +
+				", imdb='" + imdb + '\'' +
+				'}';
+	}
 }
